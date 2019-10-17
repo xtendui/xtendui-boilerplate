3cfd8231e47eead3f2c91c985ca7cacffbc8156a
Riccardo Caroli <beaver82@gmail.com>
2019-10-17 13:31:41 +0200

    chore

--- dist/assets/theme.js
+++ dist/assets/theme.js
@@ -4,10 +4,10 @@ import 'xtend-library/src/polyfill.js'
 import 'xtend-library/src/polyfill-old.js'
 import { Xt } from 'xtend-library'
 import 'xtend-library/src/xtend-core.js'
+import 'xtend-library/src/extension/test.js' // example override file: check console.debug
 import 'xtend-library/src/extension/ajax/ajax.js'
 import 'xtend-library/src/extension/smooth/smooth.js'
 import 'xtend-library/src/extension/scroll/scroll.js'
-import 'xtend-library/src/extension/test.js' // example override file: check console.debug
 import { TweenMax } from 'gsap/TweenMax'
 
 /**
