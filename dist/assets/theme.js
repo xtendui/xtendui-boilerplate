996e2e769f9d88ab709346ebf9ac0e514560a081
minimit <beaver82@gmail.com>
2020-06-21 20:25:37 +0200

    favicon

--- dist/assets/theme.js
+++ dist/assets/theme.js
@@ -6,3 +6,25 @@ import 'xtend-library/src/xtend-core.js'
 import 'xtend-library/src/addons/test.js' // example override file: check console.debug
 
 import './scripts/structure.js'
+
+/**
+ * favicon
+ */
+
+const favicon = document.querySelector('#favicon')
+const faviconDark = document.querySelector('#favicon-dark')
+
+const colorSchemeMq = window.matchMedia('(prefers-color-scheme: dark)')
+
+const changeMq = () => {
+  if (colorSchemeMq.matches) {
+    favicon.remove()
+    document.head.append(faviconDark)
+  } else {
+    document.head.append(favicon)
+    faviconDark.remove()
+  }
+}
+
+colorSchemeMq.addListener(changeMq)
+changeMq()
