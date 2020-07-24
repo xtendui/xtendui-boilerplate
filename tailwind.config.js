3beeea2abe1b7d0e12a51d6a792a94739fb07c9b
Riccardo Caroli <rcaroli@webgriffe.com>
2020-07-24 20:59:24 +0200

    chore: tailwind extend json

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,27 @@ module.exports = {
   purge: [],
   theme: require('xtend-library/src/theme')({
     extend: {
-      // components
-      btn: (theme) => ({
-        textTransform: 'capitalize',
+      // colors
+      colors: {
+        // https://javisperez.github.io/tailwindcolorshades/#/?Blue%20Ribbon=7567f8&tv=1
+        accent: {
+          100: '#F1F0FE',
+          200: '#DDD9FD',
+          300: '#C8C2FC',
+          400: '#9E95FA',
+          500: '#7567F8',
+          600: '#695DDF',
+          700: '#463E95',
+          800: '#352E70',
+          900: '#231F4A',
+        },
+      },
+      // button
+      // PROBLEM https://github.com/tailwindlabs/tailwindcss/discussions/2069
+      btn: theme => ({
+        custom: {
+          textTransform: 'capitalize',
+        },
       }),
     },
   }),
