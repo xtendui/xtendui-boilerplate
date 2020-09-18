604912f3fd2965344b1843136217b5b2fbce3442
Riccardo Caroli <rcaroli@webgriffe.com>
2020-09-18 16:09:27 +0200

    chore: tailwind theme and variant extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,76 +1,24 @@
 module.exports = {
   purge: [],
-  theme: {
-    // theme, waiting for https://github.com/tailwindlabs/tailwindcss/issues/677
-    container: {
-      center: true,
-      padding: {
-        default: '1rem',
-        sm: '1.5rem',
-        md: '2rem',
-        lg: '3rem',
-        xl: '4rem',
-      },
-    },
-    fontSize: {
-      xs: ['0.75rem', { lineHeight: '1.8' }],
-      sm: ['0.875rem', { lineHeight: '1.6' }],
-      base: ['1rem', { lineHeight: '1.6' }],
-      lg: ['1.125rem', { lineHeight: '1.5' }],
-      xl: ['1.25rem', { lineHeight: '1.4' }],
-      '2xl': ['1.5rem', { lineHeight: '1.2' }],
-      '3xl': ['1.875rem', { lineHeight: '1.2' }],
-      '4xl': ['2.25rem', { lineHeight: '1.2' }],
-      '5xl': ['3rem', { lineHeight: '1' }],
-      '6xl': ['4rem', { lineHeight: '1' }],
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: '#000',
-      white: '#fff',
-      // https://javisperez.github.io/tailwindcolorshades/#/?hawkes-blue=bdd3ff
-      gray: {
-        100: '#F8FBFF',
-        200: '#EFF4FF',
-        300: '#E5EDFF',
-        400: '#D1E0FF',
-        500: '#BDD3FF',
-        600: '#AABEE6',
-        700: '#717F99',
-        800: '#555F73',
-        900: '#393F4D',
-      },
-      // https://javisperez.github.io/tailwindcolorshades/#/?Blue%20Ribbon=7567f8&tv=1
-      accent: {
-        100: '#F1F0FE',
-        200: '#DDD9FD',
-        300: '#C8C2FC',
-        400: '#9E95FA',
-        500: '#7567F8',
-        600: '#695DDF',
-        700: '#463E95',
-        800: '#352E70',
-        900: '#231F4A',
-      },
-    },
+  theme: require('xtend-library/src/tailwind-theme')({
+    // add here your theme settings
     extend: {
-      zIndex: {
-        base: '0',
-        active: '5',
-        top: '50',
-        indicator: '51',
-        backdrop: '500',
-        drop: '600',
-        sticky: '800', // same as javascript and decreses with sequential sticky
-        overlay: '900',
-        last: '1000',
+      // add here your theme extend settings
+      // extend theme in node_modules/xtend-library/src/tailwind-theme.js
+      colors: {
+        accent: {
+          100: '#F1F0FE',
+          200: '#DDD9FD',
+          300: '#C8C2FC',
+          400: '#9E95FA',
+          500: '#7567F8',
+          600: '#695DDF',
+          700: '#463E95',
+          800: '#352E70',
+          900: '#231F4A',
+        },
       },
-      transitionTimingFunction: {
-        in: 'cubic-bezier(0.250, 0.460, 0.450, 0.940)',
-        out: 'cubic-bezier(0.455, 0.030, 0.515, 0.955)',
-      },
-      // xtend plugin
+      // extend components in node_modules/xtend-library/src/tailwind-components.js
       xtend: theme => ({
         utilities: {
           // disable utility
@@ -100,23 +48,10 @@ module.exports = {
         },
       }),
     },
-  },
-  variants: {
-    backgroundColor: ({ after }) => after(['active'], 'hover'),
-    gradientColorStops: ({ after }) => after(['active'], 'hover'),
-    backgroundOpacity: ({ after }) => after(['active'], 'hover'),
-    borderColor: ({ after }) => after(['active'], 'hover'),
-    borderOpacity: ({ after }) => after(['active'], 'hover'),
-    boxShadow: ({ after }) => after(['active'], 'hover'),
-    opacity: ({ after }) => after(['active'], 'hover'),
-    textColor: ({ after }) => after(['active'], 'hover'),
-    textOpacity: ({ after }) => after(['active'], 'hover'),
-    textDecoration: ({ after }) => after(['active'], 'hover'),
-    scale: ({ after }) => after(['active'], 'hover'),
-    rotate: ({ after }) => after(['active'], 'hover'),
-    translate: ({ after }) => after(['active'], 'hover'),
-    skew: ({ after }) => after(['active'], 'hover'),
-  },
+  }),
+  variants: require('xtend-library/src/tailwind-variants')({
+    // add here your variants
+  }),
   plugins: [require('xtend-library')],
   experimental: {
     applyComplexClasses: true,
