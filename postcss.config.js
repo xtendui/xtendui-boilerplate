1f893b53e94d242444ba89334f589a4934f7d2fe
minimit <beaver82@gmail.com>
2020-10-08 14:00:35 +0200

    chore: purgecss production

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,16 @@ const postcssImport = require(`postcss-import`)
 const postcssNesting = require('postcss-nesting')
 const tailwindcss = require(`tailwindcss`)
 
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./src/**/*.md', './src/**/*.js', './src/**/*.css'],
+  defaultExtractor: content => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+    return broadMatches.concat(innerMatches)
+  },
+})
+
 module.exports = {
-  map: true,
-  plugins: [postcssImport(), tailwindcss(), postcssNesting()],
+  plugins: [postcssImport(), tailwindcss(), postcssNesting(), ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])],
 }
