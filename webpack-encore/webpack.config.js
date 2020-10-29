ba6c21a4ebfa9fa5bc94cb79ea13e0994dda777d
minimit <beaver82@gmail.com>
2020-10-29 21:46:54 +0100

    chore

--- webpack-encore/webpack.config.js
+++ webpack-encore/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const Encore = require('@symfony/webpack-encore')
 const CopyPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
@@ -9,18 +9,27 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore.setOutputPath('public')
   .setPublicPath('/')
-  .addEntry('index', './src/index.js')
+  .addEntry('app', './src/index.js')
   .cleanupOutputBeforeBuild()
   .enableSourceMaps(!Encore.isProduction())
   .enableVersioning(Encore.isProduction())
   .enablePostCssLoader()
   .addPlugin(
     new CopyPlugin({
-      patterns: [
-        { from: path.join(__dirname, 'assets'), to: 'assets' },
-      ],
+      patterns: [{ from: path.join(__dirname, 'assets'), to: 'assets' }],
     })
   )
+  /* if using twig ignore HtmlWebpackPlugin and use
+  {% block stylesheets %}
+    {# 'app' must match the first argument to addEntry() in webpack.config.js #}
+    {{ encore_entry_link_tags('app') }}
+  {% endblock %}
+  {% block javascripts %}
+    {{ encore_entry_script_tags('app') }}
+    <script src="/build/runtime.js"></script>
+    <script src="/build/app.js"></script>
+  {% endblock %}
+  */
   .addPlugin(
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src/index.ejs'),
