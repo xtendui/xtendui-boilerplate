e97059bf2e4816b47a5a4d102e842aef2be9c15f
minimit <beaver82@gmail.com>
2020-11-17 07:55:58 +0100

    chore: animation setup

--- gulp/src/app.js
+++ gulp/src/app.js
@@ -2,15 +2,35 @@ import 'xtendui'
 import 'xtendui/src/core/drop'
 import gsap from 'gsap'
 
-// no force3d
+import { Xt } from 'xtendui'
+import 'xtendui/src/core/drop'
+import gsap from 'gsap'
+
+/**
+ * animations setup
+ */
 
 gsap.config({ force3D: false })
 
-// instant animations accessibility
-
-if (Xt.noDuration) {
+if (Xt.durationTimescale === 1000) {
+  // instant animations accessibility
   gsap.globalTimeline.timeScale(1000)
+  // double auto time accessibility
+  Xt.autoTimescale = 0.5
 }
+
+const animationResponsive = () => {
+  // faster javascript animations on small screens
+  if (Xt.durationTimescale !== 1000 && matchMedia('(max-width: 767px)').matches) {
+    gsap.globalTimeline.timeScale(1.5)
+    Xt.durationTimescale = 1.5
+  } else {
+    gsap.globalTimeline.timeScale(1)
+    Xt.durationTimescale = 1
+  }
+}
+addEventListener('resize', animationResponsive)
+animationResponsive()
 
 /**
  * favicon dark
--- webpack-encore/src/app.js
+++ webpack-encore/src/app.js
@@ -1,18 +1,34 @@
 import '../css/app.css'
 
-import 'xtendui'
+import { Xt } from 'xtendui'
 import 'xtendui/src/core/drop'
 import gsap from 'gsap'
 
-// no force3d
+/**
+ * animations setup
+ */
 
 gsap.config({ force3D: false })
 
-// instant animations accessibility
-
-if (Xt.noDuration) {
+if (Xt.durationTimescale === 1000) {
+  // instant animations accessibility
   gsap.globalTimeline.timeScale(1000)
+  // double auto time accessibility
+  Xt.autoTimescale = 0.5
 }
+
+const animationResponsive = () => {
+  // faster javascript animations on small screens
+  if (Xt.durationTimescale !== 1000 && matchMedia('(max-width: 767px)').matches) {
+    gsap.globalTimeline.timeScale(1.5)
+    Xt.durationTimescale = 1.5
+  } else {
+    gsap.globalTimeline.timeScale(1)
+    Xt.durationTimescale = 1
+  }
+}
+addEventListener('resize', animationResponsive)
+animationResponsive()
 
 /**
  * favicon dark
