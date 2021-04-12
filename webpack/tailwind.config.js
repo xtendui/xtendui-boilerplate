ca6c8316a5847c965e4aff49ddc7c0d357caa05f
Riccardo Caroli <rcaroli@webgriffe.com>
2021-04-12 11:51:57 +0200

    chore: only some colors

--- webpack/tailwind.config.js
+++ webpack/tailwind.config.js
@@ -11,7 +11,23 @@ module.exports = {
       },
     },
   },
+
   theme: {
+    // only some colors
+    colors: {
+      gray: {
+        50: '#fafafa',
+        100: '#f4f4f5',
+        200: '#e4e4e7',
+        300: '#d4d4d8',
+        400: '#a1a1aa',
+        500: '#71717a',
+        600: '#52525b',
+        700: '#3f3f46',
+        800: '#27272a',
+        900: '#18181b',
+      },
+    },
     extend: {
       colors: {
         primary: {
