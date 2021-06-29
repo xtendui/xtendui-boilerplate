4f74eeb8ce383b2ed60842aec65960800a2484fb
minimit <beaver82@gmail.com>
2021-06-29 23:43:04 +0200

    removed only some colors

--- snowpack/tailwind.config.js
+++ snowpack/tailwind.config.js
@@ -8,25 +8,6 @@ module.exports = {
     content: ['./node_modules/xtendui/src/*[!.css].js', './src/**/*.html', './src/**/*.css', './src/**/*.js'],
   },
   theme: {
-    // only some colors
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: '#000',
-      white: '#fff',
-      gray: {
-        50: '#fafafa',
-        100: '#f4f4f5',
-        200: '#e4e4e7',
-        300: '#d4d4d8',
-        400: '#a1a1aa',
-        500: '#71717a',
-        600: '#52525b',
-        700: '#3f3f46',
-        800: '#27272a',
-        900: '#18181b',
-      },
-    },
     extend: {
       // custom xtendui color
       colors: {
