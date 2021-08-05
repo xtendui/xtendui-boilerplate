e7a80a37bcaf8934c66abe8218adf5304ce1667f
Riccardo Caroli <rcaroli@webgriffe.com>
2021-08-05 10:41:47 +0200

    no gsap

--- gulp/src/app.js
+++ gulp/src/app.js
@@ -2,4 +2,4 @@ import 'xtendui'
 import 'xtendui/src/usability'
 import 'xtendui/src/drop'
 
-import './scripts/_setup'
+import './scripts/_custom'
--- vite/src/app.js
+++ vite/src/app.js
@@ -4,4 +4,4 @@ import 'xtendui'
 import 'xtendui/src/usability'
 import 'xtendui/src/drop'
 
-import './scripts/_setup'
+import './scripts/_custom'
